b1c3e265b74568e963254e41d69d2f8673949387
Konrad Grochowski <kgrochowski@n7space.com>
2026-06-09 14:00:25 +0200

    bash aliases in compilation

--- config.el
+++ config.el
@@ -87,3 +87,10 @@
   (lambda ()
     (unless (frame-focus-state)
       (save-some-buffers t))))
+
+; https://stackoverflow.com/questions/10946219/emacs-compilation-mode-wont-see-bash-alias
+(defadvice compile (around use-bashrc activate)
+  "Load .bashrc in any calls to bash (e.g. so we can use aliases)"
+  (let ((shell-command-switch "-ic"))
+    ad-do-it))
+
